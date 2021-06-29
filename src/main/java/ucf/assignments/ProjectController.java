3f6090b16d01a71f8cd290ca97925d2f82f90e19
dylanmodugno <dylanmodugno@knights.ucf.edu>
2021-06-29 16:01:27 -0400

    Updated Project

---
+++ src/main/java/ucf/assignments/ProjectController.java
@@ -0,0 +1,23 @@
+package ucf.assignments;
+
+import javafx.event.ActionEvent;
+
+public class ProjectController {
+    public void CreateClick(ActionEvent actionEvent) {
+    }
+
+    public void DeleteClick(ActionEvent actionEvent) {
+    }
+
+    public void ImportClick(ActionEvent actionEvent) {
+    }
+
+    public void ExportClick(ActionEvent actionEvent) {
+    }
+
+    public void SortClick(ActionEvent actionEvent) {
+    }
+
+    public void SelectAllClick(ActionEvent actionEvent) {
+    }
+}
